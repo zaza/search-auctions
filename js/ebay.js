1c2a567dcc4c77a42d179157887e0e5df21f3707
tomek <tomek@x200.(none)>
2014-01-05 00:41:00 +0100

    ebay script cleanup

--- js/ebay.js
+++ js/ebay.js
@@ -23,8 +23,9 @@ function Ebay(query, location) {
 }
 
 Ebay.prototype.getSearchUrl = function() {
+	var url = "http://www.ebay.pl/sch/i.html?_sacat=0&_okw=&_oexkw=&_adv=1&_udlo=&_udhi=&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_fpos=&LH_SALE_CURRENCY=0&_sop=12&_dmd=1&_ipg=50&_nkw="+this.q+"&rt=nc&LH_PrefLoc=1&_arr=1"
 	if (this.loc == null)
-		return "http://www.ebay.pl/sch/i.html?_sacat=0&_okw=&_oexkw=&_adv=1&_udlo=&_udhi=&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=200&_fpos=&LH_SALE_CURRENCY=0&_sop=12&_dmd=1&_ipg=50&_nkw="+this.q+"&rt=nc&LH_PrefLoc=1&_arr=1"
+		return url + "&_sadis=200";
 	else
-		return "http://www.ebay.pl/sch/i.html?_from=R40&_nkw="+this.q+"&_in_kw=1&_ex_kw=&_sacat=0&_okw=&_oexkw=&_adv=1&_udlo=&_udhi=&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_fspt=1&_sadis=10&_fpos="+this.loc+"&LH_SALE_CURRENCY=0&_sop=12&_dmd=1&_ipg=50";
+		return url + "_from=R40&&_in_kw=1&_ex_kw=&_fspt=1&_sadis=10&_fpos="+this.loc;
 }
